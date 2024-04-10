f2d2e68b92c33a5f4bcb7b9d3169e3af1083ae5b
Swapnil1311 <thakore.swapnil@gmail.com>
2024-04-10 20:07:49 +0530

    Updated

--- src/HireSection.jsx
+++ src/HireSection.jsx
@@ -5,7 +5,7 @@ export const HireSection = () => {
     <>
         <section className="flex justify-center items-center">
   <div className="w-full h-[300px] bg-black flex flex-col justify-center items-center">
-    <h1 className="text-5xl text-white my-3">Connect with Professionals</h1>
+    <h1 className="md:text-5xl text-3xl text-white my-3">Connect with Professionals</h1>
     <button className="bg-white text-black rounded-full px-6 py-3 mt-4 font-semibold">
       Start Your Journey
     </button>
